24df157f3b718c2af33e989ac1d82265b951d227
fahad1420 <rafahadrafahad01@gmail.com>
2026-08-26 03:01:40 +0300

    Use native TypeScript serverless entrypoint api/index.ts for Vercel rebuilds

---
+++ api/index.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import type { Request, Response } from "express";
+import app from "../server/app";
+
+export default function handler(req: Request, res: Response) {
+  return app(req, res);
+}
+
